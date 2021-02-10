bced68580ec83c5133f4df0555167380c79b8190
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2021-02-10 11:49:05 -0800

    queries

---
+++ client/src/graphql/queries.js
@@ -0,0 +1,36 @@
+import gql from "graphql-tag";
+
+export const ADD_NEW_PERSON_MUTATION = gql`
+  mutation addNewPerson($name: String!, $email: String!, $clubhouseHandle: String!) {
+    addPerson(input: {name: $name, email: $email, onBoarded: false, clubhouseHandle: $clubhouseHandle}) {
+      person {
+        email
+      }
+    }
+  }
+`;
+
+export const MARK_PERSON_AS_ONBOARDED_MUTATION = gql`
+  mutation markPersonAsOnboarded($email: String!) {
+    updatePerson(input: {filter: {email: {eq: $email}}, set: {onBoarded: true}}) {
+      numUids
+      person {
+        email
+        onBoarded
+      }
+    }
+  }
+`;
+
+const ADD_SKILLS_TO_PERSON_MUTATION = gql`
+  mutation addSkillsToPerson($email: String, $skills: [SkillRef!]) {
+    updatePerson(input: {filter: {email: {eq: $email}}, set: {skills: $skills}}) {
+      person {
+        email
+        skills {
+          name
+        }
+      }
+    }
+  }
+`;
